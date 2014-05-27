fce038f10fde4b957ae96348e3a9fa79169ac7c0
singha4 <amandeep159632478@gmail.com>
2014-05-27 10:12:35 -0400

    version 1

---
+++ TestPizza/TestPizza/Pizza.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPizza
+{
+    class Pizza
+    {
+        private string toppings;
+
+        public Pizza(double Diameter)
+        {
+            this.Diameter = Diameter;
+        }
+
+        public string Toppings
+        {
+            get
+            {
+                return toppings;
+            }
+            set
+            {
+                toppings = value;
+            }
+        }
+
+        public double Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+            set
+            {
+                diameter = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
---
+++ TestPizza/TestPizza/TestPizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestPizza
+{
+    class TestPizza
+    {
+        public static void Main()
+        {
+            Pizza pizza = new Pizza(16);
+        }
+    }
+}
